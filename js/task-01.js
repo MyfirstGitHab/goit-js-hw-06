ae1e410e4e8d26cafb8aeb208300162785dbe8a4
Alexandr <sashakaratsiupa1995@gmail.com>
2022-12-11 20:03:30 +0200

    Task 1
    OK

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+'use strict'
+const item = document.querySelectorAll('.item');
+console.log("Number of categories: "+ item.length);
+
+const listItem = document.querySelectorAll('.item');
+listItem.forEach((item,i,arry)=>{
+    const category=item.querySelector("h2").textContent;
+    console.log('Category: '+ category); 
+    const elem=item.querySelectorAll("li").length;
+    console.log('Elements: '+ elem)
+
+});
